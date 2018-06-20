e91d58ecd82abd0d446809350316402cc1e02365
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 15:01:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,7 @@ end
 def line(array)
       # binding.pry
       if array.size > 0
+            array.collect.with_index {|x, i| i+1 + x}
             print "The line is currently: "
             for x in array do
                   print "#{array.index(x) + 1}. #{x} "
