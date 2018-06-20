a82c84929c1e9740b9f832a123953a0304490aa1
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 16:56:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ end
 def line(array)
       # binding.pry
       if array.size > 0
-            puts "The line is currently:  #{array.each_with_index {|x,i| print "#{i+1}. #{x} "}}"
+            puts "The line is currently:  #{array.each_with_index {|x,i| "#{i+1}. #{x} "}}"
       else
             puts "The line is currently empty."
       end
